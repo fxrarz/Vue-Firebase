bbcfb0cb472521ff6e4e6780bacc3c0f9559f0ef
fxrarz <faru3071@yahoo.com>
2023-12-31 11:04:58 +0530

    about view is updated

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="about">
-    <h1>This is an about page. This site is hosted in firebase and managed github.</h1>
+    <h1>This is an about page. This site is hosted in firebase and managed in github.</h1>
   </div>
 </template>
